4824b2f89ecb1adf4baf447b64063b2ae0003de4
Ex1Kx <thenewestekigoh@gmail.com>
2022-06-13 10:17:26 -0500

    [Ex1] Routes For Post

--- src/app/components/Posts/post-home/post-home.component.ts
+++ src/app/components/Posts/post-home/post-home.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UserUp } from '../../../models/LoginInterfaces/user-up';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-post-home',
   templateUrl: './post-home.component.html',
   styleUrls: ['./post-home.component.scss']
 })
-export class PostHomeComponent implements OnInit {
+export class PostHomeComponent implements OnInit, OnDestroy {
+  user: UserUp[] | any;
+  subRef$!: Subscription;
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    let httpHeaders: HttpHeaders = new HttpHeaders();
+    const token = sessionStorage.getItem('token');
+    console.log('Get Token', token);
+
+    httpHeaders = httpHeaders.append('Authorization', 'Bearer' + token);
+
+    this.subRef$ = this.http.get<UserUp[]>('url', {
+      headers: httpHeaders,
+      observe: 'response'
+    }).subscribe(res => {
+      this.user = res.body;
+    },
+    err => {
+      console.log('API ERROR', err)
+    });
+  }
+  ngOnDestroy(): void {
+    if (this.subRef$) {
+      this.subRef$.unsubscribe();
+    }
   }
   logOut(){
     localStorage.removeItem('token')
